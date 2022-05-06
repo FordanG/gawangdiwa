359e73234d8bb82eadcb373ed9af0c6ba713ad63
FordanG <60175691+FordanG@users.noreply.github.com>
2022-05-07 01:32:06 +0800

    add product component

---
+++ assets/js/components/GProduct.js
@@ -0,0 +1,38 @@
+export class GProduct extends HTMLElement {
+  constructor() {
+    super();
+    this.data = "";
+  }
+
+  connectedCallback() {
+    this.data = JSON.parse(this.getAttribute("data"));
+    this.removeAttribute("data");
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `<div class="rounded-xl bg-white shadow-lg">
+            <div class="flex flex-col items-center gap-y-4">
+              <img
+                src="${this.data.imgSrc}"
+                height="354"
+                width="354"
+                class="rounded-tr-xl rounded-tl-xl"
+                alt="${this.data.name}"
+              />
+              <div class="flex flex-col items-center">
+                <p class="text-lg font-bold">${this.data.name}</p>
+                <p>PHP ${this.data.price}</p>
+              </div>
+              <button
+              id="cartButton-${this.data.id}"
+                class="w-full rounded-br-xl rounded-bl-xl bg-zinc-50 py-4 shadow-lg transition duration-100 ease-in-out hover:bg-primary hover:font-bold hover:text-white"
+              >
+                Add to Cart
+              </button>
+            </div>
+          </div>`;
+  }
+}
+
+window.customElements.define("g-product", GProduct);
